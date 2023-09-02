3f18181ab2859a62ac1c1d3b558d86d038fe2d78
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-02 14:11:14 +0100

    removed the carrying flag, now using only carrying.object to indicate which object is being carried. all tests passed.

--- collaborative_pick_and_place/macpp.py
+++ collaborative_pick_and_place/macpp.py
@@ -9,10 +9,9 @@ REWARD_COMPLETION = 20
 
 
 class Agent:
-    def __init__(self, position, picker, carrying=False, carrying_object=None):
+    def __init__(self, position, picker, carrying_object=None):
         self.position = position
         self.picker = picker
-        self.carrying = carrying
         self.carrying_object = carrying_object
         self.reward = 0
 
@@ -59,11 +58,10 @@ class MultiAgentPickAndPlace:
             )
             pygame.display.set_caption("Multi-Agent Pick and Place")
 
-            self.agent_icons = [
-                pygame.image.load("agent1.png"),  # Replace with the actual image file paths
-                pygame.image.load("agent2.png"),
-            ]
-
+            # self.agent_icons = [
+            #     pygame.image.load("icons/agent1.png"), 
+            #     pygame.image.load("icons/agent2.png"),
+            # ]
 
             
 
@@ -76,70 +74,67 @@ class MultiAgentPickAndPlace:
         all_positions = [(x, y) for x in range(self.width) for y in range(self.length)]
         random.shuffle(all_positions)
 
-        picker_flags = [True] * self.n_agents
-        random.shuffle(picker_flags)
-        for i in range(self.n_pickers, self.n_agents):
-            picker_flags[i] = False
-
-        self.objects = [
-            Object(all_positions.pop(), obj_id=i) for i in range(self.n_agents)
-        ]
+         # Initialize objects with distinct positions
+        self.objects = [Object(all_positions.pop(), obj_id=i) for i in range(self.n_agents)]
 
         # Initialize agents with distinct positions
-        self.agents = [
-            Agent(position=all_positions.pop(), picker=picker_flags[i])
-            for i in range(self.n_agents)
-        ]
+        self.agents = [Agent(position=all_positions.pop(), picker=picker_flags[i]) for i in range(self.n_agents)]
 
         # Assign goals with distinct positions
         self.goals = [all_positions.pop() for _ in range(self.n_agents)]
 
     def initialize_from_state(self, initial_state):
-        # First, initialize objects
+        # Initialise objects
         self.objects = [
             Object(position=obj["position"], obj_id=obj.get("id", None))
             for obj in initial_state["objects"]
         ]
 
-        # Then, initialize agents
+        # Initialise goals
+        self.goals = initial_state.get("goals", [])
+
+         # Initialise agents
         self.agents = []
         for i in range(self.n_agents):
             agent_x, agent_y = initial_state["agents"][i]["position"]
             picker = initial_state["agents"][i]["picker"]
 
-            # Check if "id" key exists for the object; if not, assign None
-            object_id = None
-            if i < len(
-                initial_state["objects"]
-            ):  # Ensure we're not going out of bounds
-                object_id = initial_state["objects"][i].get("id", None)
+            # Assign the carrying_object from the initial state if it exists.
+            carrying_object = initial_state["agents"][i].get("carrying_object", None)
 
             agent = Agent(
-                position=(agent_x, agent_y), picker=picker, carrying_object=object_id
+                position=(agent_x, agent_y), picker=picker, carrying_object=carrying_object
             )
             self.agents.append(agent)
 
-    def print_state(self):
-        print("Agents' Positions:")
-        for idx, agent in enumerate(self.agents):
-            print(f"- Agent {idx + 1}: Position {agent.position}")
 
-        print("Agents' Attributes:")
-        for idx, agent in enumerate(self.agents):
-            carrying_status = "Carrying" if agent.carrying else "Not Carrying"
+
+    def print_state(self):
+        print("="*40)
+        print("Agents' State:")
+        for idx, agent in enumerate(self.agents, start=1):
+            carrying_status = "Carrying" if agent.carrying_object is not None else "Not Carrying"
             carrying_object = (
-                f", Carrying Object ID: {agent.carrying_object}"
-                if agent.carrying_object is not None
-                else ""
+                f"Object ID: {agent.carrying_object}" if agent.carrying_object is not None else "None"
             )
             print(
-                f"- Agent {idx + 1}: Picker: {agent.picker}, {carrying_status}{carrying_object}"
+                f"- Agent {idx:<2}: Position: {agent.position}, "
+                f"Picker: {str(agent.picker):<5}, Status: {carrying_status:<12}, {carrying_object}"
             )
 
-        print("Goal Positions:")
-        for idx, goal in enumerate(self.goals):
-            print(f"- Goal {idx + 1}: Position {goal}")
+        print("\nObjects' Positions:")
+        for idx, obj in enumerate(self.objects, start=1):
+            print(f"- Object {idx}: Position: {obj.position}, ID: {obj.id}")  # <-- This line is changed
 
+        print("Goal Positions:")
+        if not self.goals:
+            print("- No goal positions set.")
+        else:
+            for idx, goal in enumerate(self.goals):
+                print(f"- Goal {idx + 1}: Position {goal}")
+    
+
+            
     def _random_position(self):
         return (random.randint(0, self.width - 1), random.randint(0, self.length - 1))
 
@@ -190,10 +185,10 @@ class MultiAgentPickAndPlace:
     def _handle_pickups(self):
         objects_to_remove = []
         for agent in self.agents:
-            if agent.picker and not agent.carrying:
+            print(f"Before moving: Agent at {agent.position} is carrying: {agent.carrying_object}")
+            if agent.picker and agent.carrying_object is None:
                 for obj in self.objects:
                     if obj.position == agent.position:
-                        agent.carrying = True
                         agent.carrying_object = obj.id  # Using the object's id
                         objects_to_remove.append(obj)
                         # print(
@@ -204,35 +199,43 @@ class MultiAgentPickAndPlace:
             self.objects.remove(obj)
 
     def _handle_pass_actions(self, actions):
+
+        # All agents whose action is a 'pass'
         passing_agents = [
             agent for idx, agent in enumerate(self.agents) if actions[idx] == "pass"
         ]
-        carrying_agents = [agent for agent in passing_agents if agent.carrying]
+
+        # Filter carrying agents from those who want to pass
+        carrying_agents = [agent for agent in passing_agents if agent.carrying_object is not None]
 
         for agent in carrying_agents:
             x, y = agent.position
             adjacent_positions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            
+            # Filter eligible agents from those passing adjacent to the carrying agent
             adjacent_passing_agents = [
                 adj_agent
                 for adj_agent in passing_agents
-                if adj_agent.position in adjacent_positions and not adj_agent.carrying
+                if adj_agent.position in adjacent_positions and adj_agent.carrying_object is None
             ]
 
             if adjacent_passing_agents:
                 chosen_adj_agent = random.choice(adjacent_passing_agents)
+                
+                # Give rewards only when object is passed from Picker to non-Picker
                 agent.reward += REWARD_PASS
                 chosen_adj_agent.reward += REWARD_PASS
-                agent.carrying = False
-                chosen_adj_agent.carrying = True
+                
                 chosen_adj_agent.carrying_object = agent.carrying_object
                 agent.carrying_object = None
+
 
     def _check_termination(self):
         goal_positions = set(self.goals)
         agent_positions_with_objects = [
             (agent.position, agent.carrying_object)
             for agent in self.agents
-            if agent.carrying
+            if agent.carrying_object is not None
         ]
 
         if all(
@@ -246,47 +249,32 @@ class MultiAgentPickAndPlace:
 
     def _handle_drops(self):
         for agent in self.agents:
-            if agent.carrying and not agent.picker and agent.position in self.goals:
-                if agent.carrying_object is not None:
-                    for obj in self.objects:
-                        if obj.id == agent.carrying_object:
+            if agent.carrying_object is not None:
+                print(f"Agent at {agent.position} is carrying object {agent.carrying_object}")
+                
+                if not agent.picker:
+                    print(f"Agent at {agent.position} is not a picker")
+                    
+                    if agent.position in self.goals:
+                        print(f"Agent at position {agent.position} is trying to drop object {agent.carrying_object}")
+                        
+                        obj = next((o for o in self.objects if o.id == agent.carrying_object), None)
+                        if obj:
                             obj.position = agent.position
-                            break
-                    agent.carrying_object = None
-                    agent.carrying = False
-                    agent.reward += REWARD_GOAL
+                            print(f"Object {obj.id} dropped at {obj.position}")
+                        else:
+                            print(f"No object found with ID {agent.carrying_object}")
+                        
+                        agent.carrying_object = None
+                        agent.reward += REWARD_GOAL
+                    else:
+                        print(f"Agent at {agent.position} isn't on a goal")
                 else:
-                    print(
-                        f"Warning: Agent {self.agents.index(agent) + 1} is at goal position {agent.position} and is carrying, but carrying_object is None."
-                    )
+                    print(f"Agent at {agent.position} is a picker")
+            else:
+                print(f"Agent at {agent.position} isn't carrying an object")
 
-    def get_carrying_object_position(self, agent):
-        if agent.carrying_object is not None:
-            obj_id = agent.carrying_object
-            for obj in self.objects:
-                if obj.id == obj_id:
-                    return obj.position
-        return None
 
-    def set_carrying_object_position(self, agent, new_position):
-        if agent.carrying_object is not None:
-            obj_id = agent.carrying_object
-            for obj in self.objects:
-                if obj.id == obj_id:
-                    obj.position = new_position
-                    break
-
-    def reset(self):
-        all_positions = [(x, y) for x in range(self.width) for y in range(self.length)]
-        random.shuffle(all_positions)
-
-        self.objects = [Object(all_positions.pop()) for _ in range(self.n_agents)]
-        self.agents = [
-            Agent(all_positions.pop(), picker=random.choice([True, False]))
-            for _ in range(self.n_agents)
-        ]
-        self.goals = [all_positions.pop() for _ in range(self.n_agents)]
-        self.done = False
 
     def render(self):
 
@@ -331,7 +319,7 @@ class MultiAgentPickAndPlace:
         for agent in self.agents:
             x, y = agent.position
             color = RED if agent.picker else BLUE
-            if agent.carrying:
+            if agent.carrying_object is not None:
                 # Draw an additional shape or symbol to represent carrying agents
                 pygame.draw.circle(
                     self.screen,
--- collaborative_pick_and_place/test.py
+++ collaborative_pick_and_place/test.py
@@ -1,200 +1,140 @@
-from macpp import MultiAgentPickAndPlace
 import unittest
-
+from macpp import MultiAgentPickAndPlace
 
 class TestPassingLogic(unittest.TestCase):
+
     def test_invalid_moves(self):
         initial_state = {
             "agents": [
-                {"position": (0, 0), "picker": True, "carrying": False},
-                {"position": (1, 0), "picker": False, "carrying": False},
+                {"position": (0, 0), "picker": True, "carrying_object": None},
+                {"position": (1, 0), "picker": False, "carrying_object": None},
             ],
-            "objects": [
-                {"position": (2, 2), "id": 0},
-            ],
-            "goals": [
-                (0, 0),
-                (1, 0),
-            ],
+            "objects": [{"position": (2, 2), "id": 0}],
+            "goals": [(0, 0), (1, 0)],
         }
         env = MultiAgentPickAndPlace(10, 10, 2, 0, initial_state=initial_state)
-        actions = [
-            "move_right",
-            "move_left",
-        ]
+        actions = ["move_right", "move_left"]
         env.step(actions)
-        self.assertEqual(
-            env.agents[0].position,
-            (0, 0),
-            "Agent moved out of bounds or overlapped with another agent.",
-        )
+        self.assertEqual(env.agents[0].position, (0, 0), "Agent moved out of bounds or overlapped with another agent.")
 
     def test_dropoff_object(self):
         initial_state = {
-            "agents": [{"position": (2, 2), "picker": False, "carrying": True}],
+            "agents": [{"position": (2, 2), "picker": False, "carrying_object": 0}],
             "objects": [{"position": (2, 2), "id": 0}],
             "goals": [(1, 2)],
         }
         env = MultiAgentPickAndPlace(10, 10, 1, 1, initial_state=initial_state)
+        print(env.print_state())
         actions = ["move_left"]
         env.step(actions)
-        self.assertFalse(env.agents[0].carrying, "Agent failed to drop off the object.")
+        print(env.print_state())
+        self.assertIsNone(env.agents[0].carrying_object, "Agent failed to drop off the object.")
+        self.assertEqual(env.objects[0].position, (1, 2), "Object was not dropped at the correct position.")
+
 
     def test_termination_condition(self):
-        initial_state = {
-            "agents": [
-                {"position": (3, 3), "picker": False, "carrying": True},
-                {"position": (4, 4), "picker": False, "carrying": True},
-                {"position": (5, 5), "picker": False, "carrying": True},
-                {"position": (6, 6), "picker": False, "carrying": True},
-            ],
-            "objects": [
-                {"position": (3, 3), "id": 0},
-                {"position": (4, 4), "id": 1},
-                {"position": (5, 5), "id": 2},
-                {"position": (6, 6), "id": 3},
-            ],
-            "goals": [
-                (3, 4),
-                (5, 4),
-                (6, 5),
-                (7, 6),
-            ],
-        }
-        env = MultiAgentPickAndPlace(10, 10, 4, 4, initial_state=initial_state)
-        actions = ["move_down", "move_right", "move_right", "move_right"]
-        env.step(actions)
-        self.assertTrue(env.done, "Termination condition not recognized.")
+         initial_state = {
+             "agents": [
+                 {"position": (3, 3), "picker": False, "carrying_object": 0},
+                 {"position": (4, 4), "picker": False, "carrying_object": 1},
+                 {"position": (5, 5), "picker": False, "carrying_object": 2},
+                 {"position": (6, 6), "picker": False, "carrying_object": 3},
+             ],
+             "objects": [
+                 {"position": (3, 3), "id": 0},
+                 {"position": (4, 4), "id": 1},
+                 {"position": (5, 5), "id": 2},
+                 {"position": (6, 6), "id": 3},
+             ],
+             "goals": [(3, 4), (5, 4), (6, 5), (7, 6)],
+         }
+         env = MultiAgentPickAndPlace(10, 10, 4, 4, initial_state=initial_state)
+         actions = ["move_down", "move_right", "move_right", "move_right"]
+         env.step(actions)
+         self.assertTrue(env.done, "Termination condition not recognized.")
 
     def test_pass_object_between_two_agents(self):
-        initial_state = {
-            "agents": [
-                {"position": (0, 0), "picker": True, "carrying": True},
-                {"position": (1, 0), "picker": True, "carrying": False},
-            ],
-            "objects": [{"position": (0, 0), "id": 0}],
-            "goals": [],
-        }
-        env = MultiAgentPickAndPlace(10, 10, 2, 1, initial_state=initial_state)
-        actions = ["pass", "pass"]
-        env.step(actions)
-        self.assertFalse(env.agents[0].carrying, "Agent 1 did not pass the object")
-        self.assertTrue(env.agents[1].carrying, "Agent 2 did not receive the object")
+         initial_state = {
+             "agents": [
+                 {"position": (0, 0), "picker": True, "carrying_object": 0},
+                 {"position": (1, 0), "picker": True, "carrying_object": None},
+             ],
+             "objects": [{"position": (0, 0), "id": 0}],
+             "goals": [],
+         }
+         env = MultiAgentPickAndPlace(10, 10, 2, 1, initial_state=initial_state)
+         actions = ["pass", "pass"]
+         env.step(actions)
+         self.assertIsNone(env.agents[0].carrying_object, "Agent 1 did not pass the object")
+         self.assertEqual(env.agents[1].carrying_object, 0, "Agent 2 did not receive the object")
 
     def test_multiple_agents_pick_same_object(self):
-        initial_state = {
-            "agents": [
-                {"position": (0, 0), "picker": True, "carrying": False},
-                {"position": (0, 1), "picker": True, "carrying": False},
-            ],
-            "objects": [{"position": (0, 0), "id": 0}],
-            "goals": [],
-        }
-        env = MultiAgentPickAndPlace(10, 10, 2, 2, initial_state=initial_state)
-        actions = ["pass", "move_left"]
-        env.step(actions)
-        # Only one agent should be able to pick up the object
-        self.assertTrue(
-            env.agents[0].carrying or env.agents[1].carrying,
-            "Multiple agents picked up the same object.",
-        )
-        self.assertNotEqual(
-            env.agents[0].carrying,
-            env.agents[1].carrying,
-            "Both agents managed to pick up the object.",
-        )
+         initial_state = {
+             "agents": [
+                 {"position": (0, 0), "picker": True, "carrying_object": None},
+                 {"position": (0, 1), "picker": True, "carrying_object": None},
+             ],
+             "objects": [{"position": (0, 0), "id": 0}],
+             "goals": [],
+         }
+         env = MultiAgentPickAndPlace(10, 10, 2, 2, initial_state=initial_state)
+         actions = ["pass", "move_left"]
+         env.step(actions)
+         self.assertTrue(
+             env.agents[0].carrying_object is not None or env.agents[1].carrying_object is not None,
+             "Multiple agents picked up the same object."
+         )
+         self.assertNotEqual(
+             env.agents[0].carrying_object,
+             env.agents[1].carrying_object,
+             "Both agents managed to pick up the object."
+         )
 
     def test_multiple_agent_passes(self):
-        initial_state = {
-            "agents": [
-                {"position": (0, 0), "picker": True, "carrying": True},
-                {"position": (1, 0), "picker": True, "carrying": True},
-            ],
-            "objects": [{"position": (0, 0), "id": 0}, {"position": (1, 0), "id": 1}],
-            "goals": [],
-        }
-        env = MultiAgentPickAndPlace(10, 10, 2, 2, initial_state=initial_state)
-        actions = ["pass", "pass"]
-        env.step(actions)
-        self.assertTrue(
-            env.agents[0].carrying and env.agents[1].carrying,
-            "Agents failed to handle multiple passes.",
-        )
+         initial_state = {
+             "agents": [
+                 {"position": (0, 0), "picker": True, "carrying_object": 0},
+                 {"position": (1, 0), "picker": True, "carrying_object": 1},
+             ],
+             "objects": [{"position": (0, 0), "id": 0}, {"position": (1, 0), "id": 1}],
+             "goals": [],
+         }
+         env = MultiAgentPickAndPlace(10, 10, 2, 2, initial_state=initial_state)
+         actions = ["pass", "pass"]
+         print(env.print_state())
+         env.step(actions)
+         print(env.print_state())
+         self.assertTrue(
+             env.agents[0].carrying_object is not None and env.agents[1].carrying_object is not None,
+             "Agents failed to handle multiple passes."
+         )
 
     def test_invalid_pass(self):
-        initial_state = {
-            "agents": [{"position": (0, 0), "picker": True, "carrying": True}],
-            "objects": [{"position": (0, 0), "id": 0}],
-            "goals": [],
-        }
-        env = MultiAgentPickAndPlace(10, 10, 1, 1, initial_state=initial_state)
-        actions = ["pass"]
-        env.step(actions)
-        self.assertTrue(
-            env.agents[0].carrying, "Agent passed the object when it shouldn't have."
-        )
+         initial_state = {
+             "agents": [{"position": (0, 0), "picker": True, "carrying_object": 0}],
+             "objects": [{"position": (0, 0), "id": 0}],
+             "goals": [],
+         }
+         env = MultiAgentPickAndPlace(10, 10, 1, 1, initial_state=initial_state)
+         actions = ["pass"]
+         env.step(actions)
+         self.assertEqual(env.agents[0].carrying_object, 0, "Agent passed the object when it shouldn't have.")
 
     def test_non_picker_cannot_pick(self):
-        initial_state = {
-            "agents": [{"position": (0, 0), "picker": False, "carrying": False}],
-            "objects": [{"position": (0, 0), "id": 0}],
-            "goals": [],
-        }
-        env = MultiAgentPickAndPlace(10, 10, 1, 1, initial_state=initial_state)
-        actions = ["pass"]
-        env.step(actions)
-        self.assertFalse(
-            env.agents[0].carrying,
-            "Agent that's not a picker managed to pick an object.",
-        )
+         initial_state = {
+             "agents": [{"position": (0, 0), "picker": False, "carrying_object": None}],
+             "objects": [{"position": (0, 0), "id": 0}],
+             "goals": [],
+         }
+         env = MultiAgentPickAndPlace(10, 10, 1, 1, initial_state=initial_state)
+         print(env.print_state())
+         actions = ["pass"]
+         env.step(actions)
+         print(env.print_state())
+         self.assertIsNone(env.agents[0].carrying_object, "Agent that's not a picker managed to pick an object.")
+
 
 
 if __name__ == "__main__":
 
-    # unittest.main()
-
-    initial_state = {
-        "agents": [
-            {"position": (1, 1), "picker": True, "carrying_object": 0},
-            {"position": (3, 3), "picker": False, "carrying_object": None},
-            {"position": (4, 4), "picker": True, "carrying_object": None},
-        ],
-        "objects": [{"position": (1, 0), "id": 0}],
-        "goals": [(0, 3), (3, 3), (0, 2)],
-    }
-
-    environment = MultiAgentPickAndPlace(
-        width=5,
-        length=5,
-        n_agents=3,
-        n_pickers=2,
-        initial_state=initial_state,
-        enable_rendering=True,
-    )
-
-    print(environment.print_state())
-
-    time_steps = [
-        ["move_right", "move_up", "move_left"],  # Agents move
-        ["move_down", "move_left", "move_left"],  # Agents move
-        ["move_down", "move_left", "pass"],  # Agents move and pass
-        ["move_down", "move_left", "move_left"],  # Agents move
-        ["pass", "move_left", "move_up"],  # Agents pass and move
-        ["move_left", "move_up", "move_up"],  # Agents move
-        ["move_up", "move_left", "move_up"],  # Agents move
-        ["move_up", "move_left", "move_left"],  # Agents move
-    ]
-
-    for step, actions in enumerate(time_steps):
-        rewards = environment.step(actions)
-        print(f"\nStep {step + 1} - Actions: {actions}")
-        for idx, reward in enumerate(rewards):
-            print(f"Agent {idx + 1} - Reward: {reward}")
-        environment.render()
-
-        if environment.done:
-            print("\nTask solved!")
-            break
-
-    print("Final state:")
-    environment.print_state()
+    unittest.main()
