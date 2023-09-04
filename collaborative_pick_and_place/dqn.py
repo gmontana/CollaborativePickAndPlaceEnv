d2354a4af851b26d424288c905acff13b176c726
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-04 22:01:20 +0100

    dqn uses some config files for parameters

--- collaborative_pick_and_place/dqn.py
+++ collaborative_pick_and_place/dqn.py
@@ -27,20 +27,37 @@ class ReplayBuffer:
 
 
 class DQN(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(DQN, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(input_dim, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, output_dim)
-        )
+
+    def __init__(self, env, cfg):
+        self.env = env
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        # Initialize DQN and target network
+        input_dim = env.observation_space.shape[0]
+        output_dim = env.action_space.n
+        self.policy_net = DQNNetwork(input_dim, output_dim).to(self.device)
+        self.target_net = DQNNetwork(input_dim, output_dim).to(self.device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=cfg.learning_rate)
+        self.memory = ReplayBuffer(cfg.replay_buffer_size)
+        
+        # Hyperparameters from config
+        self.batch_size = cfg.batch_size
+        self.gamma = cfg.discount_factor
+        self.epsilon = cfg.exploration_rate
+        self.epsilon_decay = cfg.exploration_decay
+        self.min_epsilon = cfg.min_exploration
+        self.target_update = cfg.target_update
 
     def forward(self, x):
         return self.fc(x)
 
-    def train(self):
+    def epsilon_greedy_actions(self, state):
+        return 0
+
+    def train(self, episodes):
 
         # Initialize DQN and target network
         policy_net = DQN(input_dim, output_dim).to(device)
@@ -51,15 +68,21 @@ class DQN(nn.Module):
         optimizer = optim.Adam(policy_net.parameters(), lr=LEARNING_RATE)
         memory = ReplayBuffer(10000)
 
-        # Training loop
         for episode in range(num_episodes):
             state = env.reset()
+            total_reward = 0
             for t in range(1000):  # or whatever max timesteps you have
                 # Select action using epsilon-greedy policy
-                # ...
+                if random.random() < epsilon:
+                    action = env.action_space.sample()
+                else:
+                    with torch.no_grad():
+                        state_tensor = torch.FloatTensor(state).to(device)
+                        q_values = policy_net(state_tensor)
+                        action = torch.argmax(q_values).item()
 
                 # Execute action in the environment
-                # ...
+                next_state, reward, done, _ = env.step(action)
 
                 # Store experience in replay buffer
                 memory.push(state, action, reward, next_state, done)
@@ -67,16 +90,21 @@ class DQN(nn.Module):
                 # Sample a batch of experiences and update DQN
                 if len(memory) > BATCH_SIZE:
                     experiences = memory.sample(BATCH_SIZE)
-                    # Convert experiences to tensors
-                    # ...
-                    
-                    # Compute Q-values and target Q-values
+                    # Convert experiences to tensors and compute Q-values and target Q-values
                     # ...
 
                     # Compute loss and update DQN
                     # ...
 
-                # Update target network
-                if episode % TARGET_UPDATE == 0:
-                    target_net.load_state_dict(policy_net.state_dict())
+                state = next_state
+                total_reward += reward
+                if done:
+                    break
+
+            # Decay epsilon
+            epsilon = max(min_epsilon, epsilon * epsilon_decay)
+
+            # Update target network
+            if episode % TARGET_UPDATE == 0:
+                target_net.load_state_dict(policy_net.state_dict())
 
