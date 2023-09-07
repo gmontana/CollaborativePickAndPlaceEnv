579e151b53f056fc96c4456216a3d249b9e04c79
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-07 19:05:24 +0100

    making wandb version work

--- experiments/cpp_qlearn.py
+++ experiments/cpp_qlearn.py
@@ -1,22 +1,27 @@
 import sys
 import wandb
 import numpy as np
-from environments.collaborative_pick_and_place.macpp import MultiAgentPickAndPlace
-from algos.q_learning import QLearning
+import os 
 
-sys.path.append('/home/gm13/Dropbox/mycode/envs/multi_agent_rl')
+PATH = os.path.expanduser("~/Dropbox/mycode/envs/multi_agent_rl")
+sys.path.append(PATH)
+
+
+from environments.collaborative_pick_and_place.macpp import MultiAgentPickAndPlace
+from algos.qlearn import QLearning
+
 
 # Configuration parameters
 config = {
     "q_table_filename": "q_table.npy",
     "cell_size": 300,
-    "env_width": 4,
-    "env_length": 4,
+    "env_width": 3,
+    "env_length": 3,
     "env_n_agents": 2,
     "env_n_pickers": 1,
-    "env_n_objects": 2,
+    "env_n_objects": 1,
     "env_enable_rendering": False,
-    "episodes": 500000,
+    "train_episodes": 150000,
     "max_steps_per_episode": 50,
     "discount_factor": 0.90,
     "min_exploration": 0.02,
@@ -24,18 +29,18 @@ config = {
     "exploration_decay": 0.99,
     "min_learning_rate": 0.01,
     "learning_rate": 0.1,
-    "learning_rate_decay": 0.995
+    "learning_rate_decay": 0.995,
 }
 
 # Initialize WandB
 wandb.init(project="cpp_qlearn", config=config)
 
-def run_training(q_learning, episodes, max_steps_per_episode):
+def run_training(q_learning, train_episodes, max_steps_per_episode):
     rewards_all_episodes = []
     successful_episodes = 0
     steps_per_episode = []
 
-    for _ in range(episodes):  
+    for _ in range(train_episodes):  
         state_hash = q_learning.env.reset()
         rewards_current_episode = 0
 
@@ -60,7 +65,7 @@ def run_experiment():
 
     cfg = wandb.config
 
-    # Initialise env
+    # Initialise environment 
     env = MultiAgentPickAndPlace(
         cell_size=cfg.cell_size,
         width=cfg.env_width,
@@ -71,7 +76,7 @@ def run_experiment():
         enable_rendering=cfg.env_enable_rendering,
     )
 
-    # Train algo
+    # Set up Q learning algorithm 
     q_learning = QLearning(
         env, 
         cfg.learning_rate, 
@@ -81,14 +86,11 @@ def run_experiment():
         cfg.min_exploration
     )
 
-    rewards_all_episodes, _ = run_training(q_learning, cfg.episodes, cfg.max_steps_per_episode)
-    wandb.log({"Rewards for All Episodes": rewards_all_episodes})
+    # Perform training
+    rewards_all_episodes, _ = run_training(q_learning, cfg.train_episodes, cfg.max_steps_per_episode)
 
     # Log rewards and other metrics
-    wandb.log({
-        "Rewards for All Episodes": rewards_all_episodes,
-        "Average Reward": np.mean(rewards_all_episodes),
-    })
+    wandb.log({"Rewards for All Episodes": rewards_all_episodes, "Average Reward": np.mean(rewards_all_episodes)})
 
     # Save and log the final policy as an artifact (if needed)
     # artifact = wandb.Artifact('model', type='model')
@@ -108,11 +110,10 @@ def run_experiment():
 
     print(f"\nExecuting learned policy over {num_episodes} episodes for statistics:")
     for _ in range(num_episodes):
-        success, total_return, total_steps = q_learning.execute(cfg.num_max_steps, save_video=True)
+        success, total_return, total_steps = q_learning.execute(cfg.max_steps_per_episode, save_video=True)
         successes.append(success)
         total_returns.append(total_return)
         total_steps_list.append(total_steps)
 
 if __name__ == "__main__":
     run_experiment()
-
