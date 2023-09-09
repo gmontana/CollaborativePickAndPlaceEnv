cb3930a31488b512730282e5ca55d871ee3b88c1
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-09 19:26:07 +0100

    execute policy and save videos

--- experiments/cpp_qlearn.py
+++ experiments/cpp_qlearn.py
@@ -34,6 +34,52 @@ config = {
 # Initialize WandB
 wandb.init(project="cpp_qlearn", name="tabular_q", config=config)
 
+def run_execution():
+
+    # Re-initialise environment 
+    env = MultiAgentPickAndPlace(
+        cell_size=cfg.cell_size,
+        width=cfg.env_width,
+        length=cfg.env_length,
+        n_agents=cfg.env_n_agents,
+        n_pickers=cfg.env_n_pickers,
+        n_objects=cfg.env_n_objects,
+        enable_rendering=True,
+        create_video=True
+    )
+
+    # Set up Q learning algorithm 
+    q_learning = QLearning(
+        env, 
+        cfg.learning_rate, 
+        cfg.discount_factor, 
+        cfg.exploration_rate, 
+        cfg.exploration_decay, 
+        cfg.min_exploration
+    )
+
+    # Load the policy
+    q_learning.q_table.load_q_table(cfg.q_table_filename)
+
+    # Create some summary stats and videos after training
+    num_episodes = 10
+    successes = []
+    total_returns = []
+    total_steps_list = []
+
+    print(f"\nExecuting learned policy over {num_episodes} episodes for statistics:")
+    for i in range(num_episodes):
+        success, total_return, total_steps = q_learning.execute(cfg.max_steps_per_episode)
+        successes.append(success)
+        total_returns.append(total_return)
+        total_steps_list.append(total_steps)
+
+        # save the video
+        timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        video_filename = f"policy_video_{i}_{timestamp}.mp4"
+        env.save_video(video_filename)
+
+
 def run_training(q_learning, train_episodes, max_steps_per_episode):
     rewards_all_episodes = []
     successful_episodes = 0
@@ -97,53 +143,11 @@ def run_experiment():
     rewards_all_episodes, _ = run_training(q_learning, cfg.train_episodes, cfg.max_steps_per_episode)
 
     # Save final policy 
-    q_table.save_q_table(cfg.q_table_filename)
+    q_learning.q_table.save_q_table(cfg.q_table_filename)
 
     # wandb.save("model.h5")
     wandb.finish()
 
-    # Re-initialise environment 
-    env = MultiAgentPickAndPlace(
-        cell_size=cfg.cell_size,
-        width=cfg.env_width,
-        length=cfg.env_length,
-        n_agents=cfg.env_n_agents,
-        n_pickers=cfg.env_n_pickers,
-        n_objects=cfg.env_n_objects,
-        enable_rendering=True,
-        create_video=True
-    )
-
-    # Set up Q learning algorithm 
-    q_learning = QLearning(
-        env, 
-        cfg.learning_rate, 
-        cfg.discount_factor, 
-        cfg.exploration_rate, 
-        cfg.exploration_decay, 
-        cfg.min_exploration
-    )
-
-    # Load the policy
-    q_table = load_q_table(cfg.q_table_filename)
-
-    # Create some summary stats and videos after training
-    num_episodes = 30
-    successes = []
-    total_returns = []
-    total_steps_list = []
-
-    print(f"\nExecuting learned policy over {num_episodes} episodes for statistics:")
-    for i in range(num_episodes):
-        success, total_return, total_steps = q_learning.execute(cfg.max_steps_per_episode)
-        successes.append(success)
-        total_returns.append(total_return)
-        total_steps_list.append(total_steps)
-
-        # save the video
-        timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        video_filename = f"policy_video_{i}_{timestamp}.mp4"
-        env.save_video(video_filename)
-
 if __name__ == "__main__":
-    run_experiment()
+    # run_experiment()
+    run_execution()
