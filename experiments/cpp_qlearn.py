43d5ce55ee18bf75cb2bb55fa3cdc185a1ba36e5
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-09 19:18:17 +0100

    execute policy and save videos

--- experiments/cpp_qlearn.py
+++ experiments/cpp_qlearn.py
@@ -2,14 +2,13 @@ import sys
 import wandb
 import numpy as np
 import os 
+import datetime
 
 PATH = os.path.expanduser("~/Dropbox/mycode/envs/multi_agent_rl")
-print(PATH)
 sys.path.append(PATH)
 
 from environments.collaborative_pick_and_place.macpp import MultiAgentPickAndPlace
 from algorithms.qlearn import QLearning
-
 
 # Configuration parameters
 config = {
@@ -97,19 +96,36 @@ def run_experiment():
     # Perform training
     rewards_all_episodes, _ = run_training(q_learning, cfg.train_episodes, cfg.max_steps_per_episode)
 
-    # Log rewards and other metrics
-    print(f"Average Reward: {np.mean(rewards_all_episodes)}")
-
-    # Save and log the final policy as an artifact (if needed)
-    # artifact = wandb.Artifact('model', type='model')
-    # artifact.add_file(cfg.q_table_filename)
-    # wandb.log_artifact(artifact)
-
     # Save final policy 
-    # q_table.save_q_table(cfg.q_table_filename)
+    q_table.save_q_table(cfg.q_table_filename)
 
-    wandb.save("model.h5")
+    # wandb.save("model.h5")
     wandb.finish()
+
+    # Re-initialise environment 
+    env = MultiAgentPickAndPlace(
+        cell_size=cfg.cell_size,
+        width=cfg.env_width,
+        length=cfg.env_length,
+        n_agents=cfg.env_n_agents,
+        n_pickers=cfg.env_n_pickers,
+        n_objects=cfg.env_n_objects,
+        enable_rendering=True,
+        create_video=True
+    )
+
+    # Set up Q learning algorithm 
+    q_learning = QLearning(
+        env, 
+        cfg.learning_rate, 
+        cfg.discount_factor, 
+        cfg.exploration_rate, 
+        cfg.exploration_decay, 
+        cfg.min_exploration
+    )
+
+    # Load the policy
+    q_table = load_q_table(cfg.q_table_filename)
 
     # Create some summary stats and videos after training
     num_episodes = 30
@@ -118,11 +134,16 @@ def run_experiment():
     total_steps_list = []
 
     print(f"\nExecuting learned policy over {num_episodes} episodes for statistics:")
-    for _ in range(num_episodes):
+    for i in range(num_episodes):
         success, total_return, total_steps = q_learning.execute(cfg.max_steps_per_episode)
         successes.append(success)
         total_returns.append(total_return)
         total_steps_list.append(total_steps)
+
+        # save the video
+        timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        video_filename = f"policy_video_{i}_{timestamp}.mp4"
+        env.save_video(video_filename)
 
 if __name__ == "__main__":
     run_experiment()
