c37d438f65fe582f2f77d02d97558d6e1a060e48
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-21 16:10:40 +0100

    defined various versions of the env to be registered

--- macpp/__init__.py
+++ macpp/__init__.py
@@ -1,8 +1,22 @@
 from gym.envs.registration import registry, register, make, spec
 from itertools import product
 
-register(
-    id="CollaborativePickAndPlace-v0",
-    entry_point="macpp.core.environment:MultiAgentPickAndPlace",
-    kwargs={"width": 3, "heigth": 3, "n_agents": 2, "n_pickers": 1},
-)
+sizes = range(3, 5)  # Grid sizes
+agents = range(2, 5)  # Number of agents
+objects = range(1, 5)  # Number of objects
+
+for s, a, o in product(sizes, agents, objects):
+    pickers = range(1, a)  # Dynamic range for pickers based on n_agents
+    for p in pickers:
+        register(
+            id="MACPP-{0}x{0}-{1}a-{2}o-{3}p-v0".format(s, a, o, p),
+            entry_point="macpp.core.environment:MultiAgentPickAndPlace",
+            kwargs={
+                "width": s,
+                "length": s,
+                "n_agents": a,
+                "n_pickers": p,
+                "n_objects": o
+            }
+        )
+
