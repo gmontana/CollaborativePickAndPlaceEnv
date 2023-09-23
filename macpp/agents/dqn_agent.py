389888539f5ea7f5525d4038e274cb8d7fb004d4
Giovanni Montana <giovanni.montana@gmail.com>
2023-09-23 15:47:49 +0100

    dqn agent first version

--- macpp/agents/dqn_agent.py
+++ macpp/agents/dqn_agent.py
@@ -1,17 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from collections import namedtuple, deque
 import random
+from exploration import EpsilonGreedy
 
 BATCH_SIZE = 64
 GAMMA = 0.99
 LEARNING_RATE = 0.001
 TARGET_UPDATE = 10
 
-Experience = namedtuple('Experience', ('state', 'action', 'reward', 'next_state', 'done'))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class ReplayBuffer:
+Experience = namedtuple('Experience', ['state', 'action', 'reward', 'next_state', 'done'])
+
+class ExperienceReplay:
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
 
@@ -26,91 +30,143 @@ class ReplayBuffer:
         return len(self.buffer)
 
 
-class DQN(nn.Module):
-
-    def __init__(self, env, cfg):
-        self.env = env
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        
-        # Initialize DQN and target network
-        input_dim = env.observation_space.shape[0]
-        output_dim = env.action_space.n
-        self.policy_net = DQNNetwork(input_dim, output_dim).to(self.device)
-        self.target_net = DQNNetwork(input_dim, output_dim).to(self.device)
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.target_net.eval()
-
-        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=cfg.learning_rate)
-        self.memory = ReplayBuffer(cfg.replay_buffer_size)
-        
-        # Hyperparameters from config
-        self.batch_size = cfg.batch_size
-        self.gamma = cfg.discount_factor
-        self.epsilon = cfg.exploration_rate
-        self.epsilon_decay = cfg.exploration_decay
-        self.min_epsilon = cfg.min_exploration
-        self.target_update = cfg.target_update
+class DQNNetwork(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DQNNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, output_dim)
 
     def forward(self, x):
-        return self.fc(x)
+        x = nn.ReLU()(self.fc1(x))
+        x = nn.ReLU()(self.fc2(x))
+        return self.fc3(x)
 
-    def epsilon_greedy_actions(self, state):
-        if random.random() < self.epsilon:
-            return self.env.action_space.sample()
+
+class DQNAgent:
+    def __init__(self, env, exploration_strategy, learning_rate=0.001, gamma=0.99, buffer_size=10000, batch_size=64, tau=0.1):
+        self.env = env
+        self.state_size = env.observation_space.shape[0]
+        self.action_size = env.action_space.n
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.network = DQNNetwork(self.state_size, self.action_size).to(device)
+        self.target_network = DQNNetwork(self.state_size, self.action_size).to(device)
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.target_network.eval()
+
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=learning_rate)
+        self.memory = ExperienceReplay(buffer_size)
+        self.exploration_strategy = exploration_strategy
+
+        self.gamma = gamma
+        self.tau = tau
+        self.batch_size = batch_size
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        if self.exploration_strategy.select_action(self, state):
+            return random.choice(range(self.action_size))
         else:
             with torch.no_grad():
-                state_tensor = torch.FloatTensor(state).to(self.device)
-                q_values = self.policy_net(state_tensor)
+                q_values = self.network(state)
                 return torch.argmax(q_values).item()
 
-    def train(self, episodes):
+    def train(self):
+        if len(self.memory) < self.batch_size:
+            return
 
-        # Initialize DQN and target network
-        policy_net = DQN(input_dim, output_dim).to(device)
-        target_net = DQN(input_dim, output_dim).to(device)
-        target_net.load_state_dict(policy_net.state_dict())
-        target_net.eval()
+        batch = self.memory.sample(self.batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
 
-        optimizer = optim.Adam(policy_net.parameters(), lr=LEARNING_RATE)
-        memory = ReplayBuffer(10000)
+        states = torch.FloatTensor(states).to(self.device)
+        actions = torch.LongTensor(actions).unsqueeze(1).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.FloatTensor(next_states).to(self.device)
+        dones = torch.BoolTensor(dones).to(self.device)
 
-        for episode in range(num_episodes):
-            state = env.reset()
-            total_reward = 0
-            for t in range(1000):  # or whatever max timesteps you have
-                # Select action using epsilon-greedy policy
-                if random.random() < epsilon:
-                    action = env.action_space.sample()
-                else:
-                    with torch.no_grad():
-                        state_tensor = torch.FloatTensor(state).to(device)
-                        q_values = policy_net(state_tensor)
-                        action = torch.argmax(q_values).item()
+        current_q_values = self.network(states).gather(1, actions)
+        next_q_values = self.target_network(next_states).max(1)[0].detach()
+        target_q_values = rewards + (self.gamma * next_q_values * (~dones))
 
-                # Execute action in the environment
-                next_state, reward, done, _ = env.step(action)
+        loss = F.mse_loss(current_q_values, target_q_values.unsqueeze(1))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-                # Store experience in replay buffer
-                memory.push(state, action, reward, next_state, done)
+    def update_target_network(self):
+        self.target_network.load_state_dict(self.network.state_dict())
 
-                # Sample a batch of experiences and update DQN
-                if len(memory) > BATCH_SIZE:
-                    experiences = memory.sample(BATCH_SIZE)
-                    # Convert experiences to tensors and compute Q-values and target Q-values
-                    # ...
+    def decay_epsilon(self):
+        self.exploration_strategy.decay_exploration_rate()
 
-                    # Compute loss and update DQN
-                    # ...
 
-                state = next_state
-                total_reward += reward
-                if done:
-                    break
+def game_loop(env, agent, training=True, num_episodes=10000, max_steps_per_episode=300, render=False, model_file='dqn_model'):
+    total_rewards = []
+    failure_count = 0
+    for episode in range(num_episodes):
+        state = env.reset()
+        episode_reward = 0
+        for step in range(max_steps_per_episode):
+            if render:
+                env.render()
 
-            # Decay epsilon
-            epsilon = max(min_epsilon, epsilon * epsilon_decay)
+            action = agent.select_action(state)
+            next_state, reward, done, _ = env.step(action)
 
-            # Update target network
-            if episode % TARGET_UPDATE == 0:
-                target_net.load_state_dict(policy_net.state_dict())
+            if training:
+                agent.memory.push(state, action, reward, next_state, done)
+                agent.train()
+
+            episode_reward += reward
+            state = next_state
+
+            if done:
+                break
+
+            if step >= max_steps_per_episode:
+                failure_count +=1
+
+        total_rewards.append(episode_reward)
+
+        # Decay epsilon for exploration-exploitation trade-off
+        if training:
+            agent.decay_epsilon()
+
+        # Periodically update the target network
+        if training and episode % 100 == 0:
+            agent.update_target_network()
+
+        # Logging
+        if episode % 100 == 0:
+            avg_reward = sum(total_rewards[-100:]) / 100
+            success_rate = 1 - (failure_count / 100)
+            print(f"Episode {episode}/{num_episodes}: Avg Reward: {avg_reward:.2f}, Success rate: {success_rate:.2f}, Epsilon: {agent.exploration_strategy.epsilon:.3f}")
+
+        # Save the model periodically
+        if training and episode % 1000 == 0:
+            torch.save(agent.network.state_dict(), model_file + f"_{episode}.pth")
+
+    if training:
+        torch.save(agent.network.state_dict(), model_file + "_final.pth")
+
+
+if __name__ == "__main__":
+
+    from macpp.core.environment import MACPPEnv
+
+    # Set up the environment
+    env = MACPPEnv(
+        grid_size=(3, 3), n_agents=2, n_pickers=1, n_objects=1, cell_size=300, debug_mode=False
+    )
+
+    # Set up exploration strategy
+    epsilon_greedy_strategy = EpsilonGreedy(exploration_rate=1.0, min_exploration=0.02, exploration_decay=0.99)
+    # ucb_strategy = UCB(c=5)
+
+    # Set up the agent
+    agent = DQNAgent(env, epsilon_greedy_strategy,learning_rate=0.001, gamma=0.99, buffer_size=10000, batch_size=64, tau=0.1)
+
+    # Train the agent
+    game_loop(env, agent, training=True, num_episodes=5000, max_steps_per_episode=500, render=False, model_file='dqn_model')
 
