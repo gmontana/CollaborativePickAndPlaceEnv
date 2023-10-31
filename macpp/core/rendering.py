6aa138a7432e4a3099566d607e27f42ce7171501
Giovanni Montana <giovanni.montana@gmail.com>
2023-10-31 18:16:18 +0000

    refactored rendering: improved efficiency (images resized only once, exception handling, more efficient entity drawing)

--- macpp/core/rendering.py
+++ macpp/core/rendering.py
@@ -5,8 +5,6 @@ import pygame
 import os
 
 WHITE = (255, 255, 255)
-GRAY = (200, 200, 200)
-BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
@@ -23,34 +21,47 @@ class Viewer:
              self.env.grid_length * self.env.cell_size)
         )
 
-        base_path = os.path.dirname(__file__)
-        icon_path = os.path.join(base_path, "icons")
-        self.picker_icon = self._load_image(
-            os.path.join(icon_path, "picker_not_carrying.png"))
-        self.picker_carrying_icon = self._load_image(
-            os.path.join(icon_path, "picker_carrying.png"))
-        self.non_picker_with_box_icon = self._load_image(
-            os.path.join(icon_path, "dropper_not_carrying_with_box.png"))
-        self.non_picker_icon = self._load_image(
-            os.path.join(icon_path, "dropper_not_carrying.png"),)
-        self.non_picker_carrying_icon = self._load_image(
-            os.path.join(icon_path, "dropper_carrying.png"))
-        self.object_icon = self._load_image(
-            os.path.join(icon_path, "box.png"))
-
         self.screen = pygame.display.set_mode(
             (self.env.grid_width * self.env.cell_size,
              self.env.grid_length * self.env.cell_size)
         )
         pygame.display.set_caption("Collaborative Multi-Agent Pick and Place")
 
-    def _load_image(self, image_path):
+        # Pre-calculate the icon size based on the cell size
+        self.icon_size = int(self.env.cell_size * 0.8)
+
+        base_path = os.path.dirname(__file__)
+        icon_path = os.path.join(base_path, "icons")
+
+        # Load and resize images once during initialization
+        self.picker_icon = self._load_and_resize_image(
+            os.path.join(icon_path, "picker_not_carrying_large_new.png"))
+        self.picker_carrying_icon = self._load_and_resize_image(
+            os.path.join(icon_path, "picker_carrying_large_new.png"))
+        self.non_picker_with_box_icon = self._load_and_resize_image(
+            os.path.join(icon_path, "dropper_not_carrying_with_box_large.png"))
+        self.non_picker_icon = self._load_and_resize_image(
+            os.path.join(icon_path, "dropper_not_carrying_large.png"))
+        self.non_picker_carrying_icon = self._load_and_resize_image(
+            os.path.join(icon_path, "dropper_carrying_large.png"))
+        self.object_icon = self._load_and_resize_image(
+            os.path.join(icon_path, "box_large.png"))
+
+
+    def _load_and_resize_image(self, image_path):
         try:
-            return pygame.image.load(image_path)
+            image = pygame.image.load(image_path)
+            return pygame.transform.scale(image, (self.icon_size, self.icon_size))
         except FileNotFoundError:
             raise FileNotFoundError(
                     f"Error: Image file {image_path} not found."
             )
+
+    def _grid_to_screen(self, grid_x, grid_y):
+        """Convert grid coordinates to screen coordinates."""
+        x = grid_x * self.env.cell_size + self.env.cell_size // 2
+        y = grid_y * self.env.cell_size + self.env.cell_size // 2
+        return x, y
 
     def _draw_grid(self):
         for x in range(0, self.env.grid_width * self.env.cell_size, self.env.cell_size):
@@ -68,14 +79,6 @@ class Viewer:
                 (self.env.grid_width * self.env.cell_size, y),
             )
 
-    def _draw_agent_icon(self, agent_icon, cell_center, icon_size):
-        try:
-            agent_icon_resized = pygame.transform.scale(agent_icon, (icon_size, icon_size))
-            agent_icon_rect = agent_icon_resized.get_rect(center=cell_center)
-            self.offscreen_surface.blit(agent_icon_resized, agent_icon_rect)
-        except Exception as e:
-            print(f"Error loading icon: {e}")
-
     def _get_agent_icon(self, agent, can_display_side_by_side):
         if can_display_side_by_side:
             return self.non_picker_with_box_icon
@@ -83,47 +86,38 @@ class Viewer:
             return self.picker_carrying_icon if agent.carrying_object is not None else self.picker_icon
         return self.non_picker_carrying_icon if agent.carrying_object is not None else self.non_picker_icon
 
-    def _draw_agents(self):
-        cell_size = self.env.cell_size  
-        icon_size = int(cell_size * 0.8)
+    def _draw_agent_icon(self, agent_icon, cell_center):
+        try:
+            agent_icon_rect = agent_icon.get_rect(center=cell_center)
+            self.offscreen_surface.blit(agent_icon, agent_icon_rect)
+        except Exception as e:
+            print(f"Error loading icon: {e}")
 
+    def _draw_rect(self, color, x, y, width, height, thickness=0):
+        """Draw a rectangle on the offscreen surface."""
+        rect = pygame.Rect(x - width // 2, y - height // 2, width, height)
+        pygame.draw.rect(self.offscreen_surface, color, rect, thickness)
+
+    def _draw_agents(self):
         for agent in self.env.agents:
-            x, y = agent.position
-            cell_center = (x * cell_size + cell_size // 2, y * cell_size + cell_size // 2)
-            
+            x, y = self._grid_to_screen(*agent.position)
             object_at_position = any(obj.position == agent.position for obj in self.env.objects)
             can_display_side_by_side = not agent.picker and agent.carrying_object is None and object_at_position
             agent_icon = self._get_agent_icon(agent, can_display_side_by_side)
-            
-            self._draw_agent_icon(agent_icon, cell_center, icon_size)
+            self._draw_agent_icon(agent_icon, (x, y))
 
     def _draw_objects(self):
         for obj in self.env.objects:
-            x, y = obj.position
-            img_size = int(self.env.cell_size * 0.8)
-            obj_img_resized = pygame.transform.scale(
-                self.object_icon, (img_size, img_size))
-            pos = (x * self.env.cell_size + self.env.cell_size // 2 - img_size // 2,
-                   y * self.env.cell_size + self.env.cell_size // 2 - img_size // 2)
-            self.offscreen_surface.blit(obj_img_resized, pos)
-
-    def _draw_tick_border(self, x, y, color, thickness=8):
-        cell_size = self.env.cell_size
-        rect = pygame.Rect(x * cell_size, y * cell_size, cell_size, cell_size)
-        pygame.draw.rect(self.offscreen_surface, color, rect, thickness)
+            x, y = self._grid_to_screen(*obj.position)
+            self.offscreen_surface.blit(self.object_icon, (x - self.icon_size // 2, y - self.icon_size // 2))
 
     def _draw_goals(self):
         for goal in self.env.goals:
-            x, y = goal
-            object_at_goal = any(obj.position == (x, y) for obj in self.env.objects)
-            dropper_at_goal = any(agent.position == (x, y) and not agent.picker for agent in self.env.agents)
-            
-            if object_at_goal and (not any(agent.position == (x, y) for agent in self.env.agents) or dropper_at_goal):
-                border_color = GREEN
-            else:
-                border_color = RED
-                
-            self._draw_tick_border(x, y, border_color)
+            x, y = self._grid_to_screen(*goal)
+            object_at_goal = any(obj.position == goal for obj in self.env.objects)
+            dropper_at_goal = any(agent.position == goal and not agent.picker for agent in self.env.agents)
+            border_color = GREEN if object_at_goal and (not any(agent.position == goal for agent in self.env.agents) or dropper_at_goal) else RED
+            self._draw_rect(border_color, x, y, self.env.cell_size, self.env.cell_size, thickness=8)
 
 
     def render(self):
